cf5b2b73b7929e291f40a7f4cf6027e8f34f1cd8
liruihao <liruihao2008@gmail.com>
2016-02-01 02:13:02 +0000

    registrated

--- SLAM_Record.py
+++ SLAM_Record.py
@@ -18,7 +18,7 @@ class image_converter():
 #        cv2.namedWindow('Image depth')
         self.bridge = CvBridge()
         self.image_sub_color = rospy.Subscriber("/camera/rgb/image_raw",Image, self.callback_color)
-        self.image_sub_depth = rospy.Subscriber("/camera/depth/image_raw", Image, self.callback_depth)
+        self.image_sub_depth = rospy.Subscriber("/camera/depth_registered/image_raw", Image, self.callback_depth)
         self.groundtruth_sub = rospy.Subscriber("/vicon/Xtion/Xtion", TransformStamped, self.callback_groundtruth)
         rospy.rostime.set_rostime_initialized( True )
         print 'SALM recorder is waiting for Image and Vicon.'
@@ -51,7 +51,7 @@ class image_converter():
 	qw = data.transform.rotation.w
         groundtruthfile = open('./Data/groundtruth.txt', 'a')	
 	time = rospy.rostime.get_time()
-        print >> groundtruthfile, "%f %f %f %f %f %f %f %f" %(time, wx, wy, wz, qx, qy, qz, qw)
+        print >> groundtruthfile, "%.4f %.4f %.4f %.4f %.4f %.4f %.4f %.4f" %(time, wx, wy, wz, qx, qy, qz, qw)
     
 if __name__=='__main__':
     ic = image_converter()
